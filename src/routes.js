157473070d32b2e9017a2f8a2427d5836e72eee9
Álvaro Oliveira <alvarogab6@gmail.com>
2020-01-25 13:46:22 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import All from './pages/All';
+import Profile from './pages/Profile';
+import RegisterAnimal from './pages/RegisterAnimal';
 
 const Routes = createAppContainer(
     createStackNavigator({
@@ -28,6 +30,18 @@ const Routes = createAppContainer(
         },
         All: {
             screen: All,
+            navigationOptions: {
+                headerShown: false,
+            }
+        },
+        Profile: {
+            screen: Profile,
+            navigationOptions: {
+                headerShown: false,
+            }
+        },
+        RegisterAnimal: {
+            screen: RegisterAnimal,
             navigationOptions: {
                 headerShown: false,
             }
